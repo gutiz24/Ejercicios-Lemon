4f635ce4302b52ee81b3d01348b27b238dafb845
gutiz24 <vagrant@ubuntu-client>
2024-10-15 17:17:37 +0000

    añdido ejercicio4.sh

--- 00-fundamentos-linux/ejercicio4.sh
+++ 00-fundamentos-linux/ejercicio4.sh
@@ -2,5 +2,16 @@
 
 URL='https://poetrydb.org/author/Shakespeare/title'
 
-curl $URL
+REQUEST=$(curl -s $URL)
 
+OCURRENCE_COUNT=$(echo "$REQUEST"  | grep -c $1 2> /dev/null)
+
+if [ "$OCURRENCE_COUNT" == 0 ]; then
+	echo no se ha encontrado la palabra $1
+	exit 1
+fi
+
+FIRST_LINE_OCURRENCE=$(echo "$REQUEST" | grep -n $1 | tail -1 | cut -f1 -d: )
+
+echo la palabra $1 aparece $OCURRENCE_COUNT veces
+echo Aparece por primera vez en la línea $FIRST_LINE_OCURRENCE
